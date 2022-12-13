781f4cbe1938d268060777a61c0883228ce27d75
Anthony Drean <anthony.drean@gmail.com>
2022-12-13 23:13:36 +0100

    2021 - day 15 - scala - part 2

--- scala/src/main/scala/Year21day15.scala
+++ scala/src/main/scala/Year21day15.scala
@@ -13,24 +13,43 @@ class ChitonsExplorer(val chitons:Array[Array[Int]]) {
   val yMax: Int = chitons.last.length
 
   def run(): Unit = {
-    val start:(Int,Int) = (0,0)
-    follow((start._1 + 1, start._2), 0)
-    follow((start._1 + 1, start._2), 0)
+    var coords: List[((Int,Int), Int)] = List[((Int,Int), Int)]()
+    coords :+= ((0,0), 0)
+    var cpt:Int = 0
+    while coords.length > 0 do
+      cpt += 1
+      coords = coords.flatMap((coord, risk) =>
+        var newCoords:List[((Int,Int), Int)] = List[((Int,Int), Int)]()
+        var coords:Array[(Int,Int)] = Array((coord._1 - 1, coord._2),(coord._1 + 1, coord._2),(coord._1, coord._2 - 1),(coord._1, coord._2 + 1))
+          coords
+            .filter((x:Int,y:Int) => x >= 0 && y >= 0 && x < xMax && y < yMax)
+            .filter((x,y) => lowestRisks((x,y)) > risk + chitons(x)(y))
+              .map((x,y) =>
+                val newRisk = risk + chitons(x)(y)
+                lowestRisks((x,y)) = newRisk
+                ((x,y), newRisk)
+              )
+      ).groupBy(_._1).map(x => (x._1,x._2.minBy(_._2)._2)).toList
+    println(lowestRisks(xMax - 1, yMax - 1))
   }
 
-  def follow(coord:(Int,Int), risk:Int): Unit = {
+  private def follow(coord:(Int,Int), risk:Int): Unit = {
     val newrisk: Int = risk + chitons(coord._1)(coord._2)
     if newrisk < lowestRisks(coord) then
       lowestRisks(coord) = newrisk
-    else return
-    if coord._1 < xMax - 1 then
-      follow((coord._1 + 1, coord._2), newrisk)
-    if coord._2 < yMax - 1 then
-      follow((coord._1, coord._2 + 1), newrisk)
-    if coord == (xMax - 1, yMax - 1) then
-      if newrisk < lowestRiskPath then
-        println(s"Found new safest path with risk of $newrisk")
-        lowestRiskPath = newrisk
+      if coord == (xMax - 1, yMax - 1) then
+        if newrisk < lowestRiskPath then
+          println(s"Found new safest path with risk of $newrisk")
+          lowestRiskPath = newrisk
+      else
+        if coord._1 < xMax - 1 then
+          follow((coord._1 + 1, coord._2), newrisk)
+        if coord._1 > 0 then
+          follow((coord._1 - 1, coord._2), newrisk)
+        if coord._2 < yMax - 1 then
+          follow((coord._1, coord._2 + 1), newrisk)
+        if coord._2 > 0 then
+          follow((coord._1, coord._2 - 1), newrisk)
   }
 
 }
@@ -44,6 +63,20 @@ object Year21day15 {
       .getLines()
       .map(line => line.toCharArray.map(_.asDigit)).
       toArray
+
     ChitonsExplorer(chitons).run()
+
+    var fiveChitons = chitons.map(line => (0 to 4).toArray.flatMap(k => line.map(chiton =>
+      var value = chiton + k
+      if value > 9 then value -= 9
+      value
+    )))
+    fiveChitons = (0 to 4).flatMap(k => fiveChitons.map(_.map(chiton =>
+      var value = chiton + k
+      if value > 9 then value -= 9
+      value
+    ))).toArray
+    ChitonsExplorer(fiveChitons).run()
+
   }
 }
